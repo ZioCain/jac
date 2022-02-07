1ad624a6bf4e074fffff91d1bd6aea337d26f367
ZioCain <micael.castelleits@gmail.com>
2022-02-07 11:12:22 +0100

    Eliminazione dati

---
+++ 2022-02-07/dati.js
@@ -0,0 +1,15 @@
+var dati = [
+    {
+		id: 1,
+		frase: "asdfasdfasdf"
+	},
+	{
+		id:2,
+		frase: "frase 2"
+	},
+	{
+		id: 3,
+		frase: "frase 3"
+	}
+];
+
--- 2022-02-07/main.js
+++ 2022-02-07/main.js
@@ -1,23 +1,43 @@
-var dati = [
-    "<b>asdf</b>asfd",
-    "asdfasdf",
-    "valore 3",
-    "valore 4",
-    "frase 5",
-	"asdfasdf",
-	"opgofddhjdf"
-];
-
 const corpo = document.getElementById("corpo-tabella");
+
+function Elimina(elem){
+	// cerco l'indice dell'elemento
+	const indice = dati.findIndex((x)=>x.id == elem.id);
+	// se non trovo l'elemento, termino l'esecuzione
+	if(indice === -1) return;
+	// rimuovo elemento dall'array
+	dati.splice(indice, 1);
+
+	// rimuovo elemento dall'HTML
+	document.getElementById(elem.id).remove();
+}
+
+function AggiungiAzioni(tr, elem){
+	var td = document.createElement("td");
+	var btn = document.createElement("button");
+
+	btn.innerText = "Cancella";
+	tr.appendChild(td);
+	td.appendChild(btn);
+
+	btn.onclick = function(){Elimina(elem)};
+}
+
+function AggiungiARiga(tr, elem){
+	var td = document.createElement("td");
+	tr.appendChild(td);
+	td.innerText = elem;
+}
 
 function AggiungiATabella(elem){
 	var tr = document.createElement("tr");
-	var td = document.createElement("td");
-
 	corpo.appendChild(tr);
-	tr.appendChild(td);
+	tr.id = elem.id;
+	for(var key in elem){
+		AggiungiARiga(tr, elem[key]);
+	}
 
-	td.innerText = elem;
+	AggiungiAzioni(tr, elem);
 }
 
 dati.forEach( AggiungiATabella );
